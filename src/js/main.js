99d7117178533cae9072c036faa9b20ccb4df87f
Louisa Reese <louisa.e.reese@gmail.com>
2016-01-29 15:04:04 -0700

    added styling, able to access and display API information

--- src/js/main.js
+++ src/js/main.js
@@ -2,11 +2,36 @@
 
 $(document).on('ready', function() {
   console.log('sanity check!');
+
+  $('form').on("submit", function(event){
+  	event.preventDefault();
+  	$("#results").empty();
+  	var keyword = $("#search").val();
+  	getEvents(keyword);
+  })
 });
 
-$.get()
+
+// keyword search
+// filter after searching based on category
+// filter by price
+// add to their favorite's section
+
+//make categories static
+// when clicked, their id will link to a new search
 
 
 
+function getEvents(keyword){
 
+var url = 'https://www.eventbriteapi.com/v3/categories/?token=YGUB3C37CXPEQAMTUJ4C';
+
+	$.get(url).success(function(response){
+		console.log(response);
+			response.categories.forEach(function(value){
+					console.log(value.name);
+					$("#results").append('<li data-id="'+value.id+'"><a href="#">'+value.name+'</a></li>');
+		});
+	});
+}
 
