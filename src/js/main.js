a45e91ad20f098ea9087b6f75025484a9901811a
Louisa Reese <louisa.e.reese@gmail.com>
2016-01-30 16:35:38 -0700

    more changes

--- src/js/main.js
+++ src/js/main.js
@@ -8,7 +8,7 @@ $(document).on('ready', function() {
   	$("#results").empty();
   	var keyword = $("#search").val();
   	getEvents(keyword);
-  })
+  });
 });
 
 
@@ -20,18 +20,32 @@ $(document).on('ready', function() {
 //make categories static
 // when clicked, their id will link to a new search
 
+ // genreurl ='https://www.eventbriteapi.com/v3/categories/103/?token=YGUB3C37CXPEQAMTUJ4C'
 
 
-function getEvents(keyword){
 
-var url = 'https://www.eventbriteapi.com/v3/categories/?token=YGUB3C37CXPEQAMTUJ4C';
+// function getEvents(keyword){
+// var url = 'https://www.eventbriteapi.com/v3/categories/?token=YGUB3C37CXPEQAMTUJ4C';
 
+// 	$.get(url).success(function(response){
+// 		console.log(response);
+// 			response.categories.forEach(function(value){
+// 					console.log(value.name);
+// 					$("#results").append('<li data-id="'+value.id+'"><a href="#">'+value.name+'</a></li>');
+// 		});
+// 	});
+// }
+
+function getEvents(){
+	var url = 'https://www.eventbriteapi.com/v3/categories/103/?token=YGUB3C37CXPEQAMTUJ4C';
 	$.get(url).success(function(response){
-		console.log(response);
-			response.categories.forEach(function(value){
-					console.log(value.name);
-					$("#results").append('<li data-id="'+value.id+'"><a href="#">'+value.name+'</a></li>');
+				console.log(response);
+			response.subcategories.forEach(function(value){
+				$("#results").append('<li data-id="'+value.name+'"><a href="#">'+value.name+'</a></li>');
+			});
 		});
-	});
-}
+	}
+
+
+
 
