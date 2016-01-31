1adaaeb6de3e3fd328f36c7b7a4661a4e1082c9f
Louisa Reese <louisa.e.reese@gmail.com>
2016-01-31 12:26:02 -0700

    artist and thumbnail displayed on page, centered

--- src/js/main.js
+++ src/js/main.js
@@ -1,40 +1,67 @@
 // add scripts
-
 $(document).on('ready', function() {
   console.log('sanity check!');
 });
 
-  $('form').on("submit", function(event){
+  $('#citySearch').on("submit", function(event){
   	event.preventDefault();
-  	$("#results").empty();
+  	$('#cityResults').empty();
   var city = $('#city').val();
 	var state = $('#state').val();
 	var miles = $('#miles').val();
-  	getEvents(city, state, miles);
-  });
+  getEvents(city, state, miles);
+});
 
+$('#artistSearch').on("submit", function(event){
+	event.preventDefault();
+	$('#artistResults').empty();
+		var artist = $('#artist').val();
+		searchArtist(artist);
+});
+
+
+
+// search by city
 
 function getEvents(city, state, miles) {
+	var url = "https://api.bandsintown.com/events/search.json?api_version=2.0&app_id=WHATS_UP&location="+city+","+state+"&radius="+miles;
 
-	var url = "https://api.bandsintown.com/events/search.json?app_id=WHATS_UP_DENVER&location="+city+","+state+"&radius="+miles;
+var settings = {
+	  "async": true,
+	  "crossDomain": true,
+	  "dataType": "jsonp",
+	  "url": url,
+	  "method": "GET",
+	};
+
+	$.ajax(settings).done(function(response) {
+		console.log(response);
+			return response.filter(function(value){
+				return value;
+		}).forEach(function(val){
+			console.log(val);
+			$('#cityResults').append('<li data-id="'+val.artists[0].name+'">'+'Date: ', val.datetime, ' Artist: ', val.artists[0].name+'</li>');
+		});
+	});
+}
+
+// search by artist
+
+function searchArtist(artist){
+	var url = "http://api.bandsintown.com/artists/"+artist+".json?api_version=2.0&app_id=WHATS_UP"
+
 	var settings = {
 	  "async": true,
 	  "crossDomain": true,
 	  "dataType": "jsonp",
 	  "url": url,
 	  "method": "GET",
-	}
+	};
 
-	$.ajax(settings).done(function(response) {
-			return response.filter(function(value){
-				return value;
-		}).forEach(function(val){
-			$('#results').append('<hr><li data-id="'+val.artists[0].name+'"><'+'Date: ', val.datetime, 'Artist: ', val.artists[0].name+'</li>');
+	$.ajax(settings).done(function(response){
+		$("#artistResults").append('<li>' +response.name+ '<br><img src=' +response.thumb_url+ '></li>');
 		});
-	});
-}
-
-// artist
+};
 
 
 
